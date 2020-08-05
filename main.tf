694387221953928e28c629f3dda49a16a13d86fc
Rajeev Jaggavarapu <rajeev.jaggavarapu@srijan.net>
2020-08-05 11:34:44 +0530

    Disabling flow logs

--- main.tf
+++ main.tf
@@ -2,4 +2,5 @@ module "vpc" {
   source           = "app.terraform.io/foss-cafe/vpc/aws"
   version          = "1.0.0"
   cidr_block       = var.cidr_block
+  enable_flow_logs = var.enable_flow_logs
 }
